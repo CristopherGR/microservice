7ce405da884edb66fbf21f6f2b494cc6b2d2826f
Cristopher <creitous@gmail.com>
2023-01-19 02:13:22 -0500

    client terminado

--- src/main/java/com/devsu/microservice/controller/ClientController.java
+++ src/main/java/com/devsu/microservice/controller/ClientController.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.devsu.microservice.service.ClientService;
 import com.devsu.microservice.service.dto.ClientDto;
 import com.devsu.microservice.utils.ResponseMessage;
+
+import jakarta.transaction.Transactional;
 
 @RestController
 @CrossOrigin(origins="*")
@@ -33,5 +37,14 @@ public class ClientController {
 	public ResponseEntity<ResponseMessage> edit(@RequestBody ClientDto clientDto){
 		ResponseMessage responseMessage = clientService.edit(clientDto);
 		return new ResponseEntity<>(responseMessage, new HttpHeaders(), HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/cuentas/eliminar/{id}")
+	@Transactional
+	public ResponseEntity<ResponseMessage> delete(@PathVariable("id") Long idClient) {
+		ResponseMessage responseMessage;
+		responseMessage = clientService.delete(idClient);
+		return new ResponseEntity<>(responseMessage, new HttpHeaders(), HttpStatus.OK);
+
 	}
 }
